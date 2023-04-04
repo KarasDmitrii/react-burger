fdecaff5b0ae07d6d509ed1e6ecd31b5d0820298
KarasDmitrii <dimakaras83@gmail.com>
2023-04-04 13:57:05 +0400

    исправлено поведение protectedRoute

--- src/components/protectedRoutes/ProtectedRoutes.tsx
+++ src/components/protectedRoutes/ProtectedRoutes.tsx
@@ -1,7 +1,9 @@
 import { Navigate, useLocation, } from "react-router-dom";
 import { getIsAuthChecked, getIsLoggedIn } from '../../services/protected-router/protecetRouteSelectors';
-import { useAppSelector } from "../../hooks/hooks";
+import { useAppSelector, useDispatch } from "../../hooks/hooks";
 import { Preloader } from "../preloader/Preloader";
+import { useEffect } from "react";
+import { checkUserAuth } from "../../services/user/UserAction";
 
 
 interface IprotecedRoute {
@@ -15,8 +17,13 @@ const ProtectedRoute: React.FC<IprotecedRoute> = ({ children, anonymous = false 
   const isAutnChecked = useAppSelector(getIsAuthChecked)
   const location = useLocation();
   const from = location.state?.from?.pathname || '/';
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(checkUserAuth())
+  }, [])
 
   if (!isAutnChecked) {
+    
     return (<Preloader/>);
   }
 
