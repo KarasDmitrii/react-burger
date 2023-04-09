0c899a17daba53a5c0cc7ba553cc28f3bc5d5173
KarasDmitrii <dimakaras83@gmail.com>
2023-04-10 00:35:05 +0400

    исправлены замечания

--- src/services/IngredientDetails/ingredientDetails.test.js
+++ src/services/IngredientDetails/ingredientDetails.test.js
@@ -1,26 +1,22 @@
-import { ingredientDetailsReducer } from './IngredientDetailsReducer';
+import { ingDetailsInitialState, ingredientDetailsReducer } from './IngredientDetailsReducer';
 import { CLOSE_ING_MODAL, OPEN_ING_MODAL } from './IngedientDetailsActions';
 
 describe('IngredientDetailsReducer', () => {
-  const initialState = {
-    isIngModalOpen: false,
-    item: null,
-    header: 'Детали ингредиента',
-  };
+  
   
   it('should return initial state', () => {
-    expect(ingredientDetailsReducer(undefined, {})).toEqual(initialState);
+    expect(ingredientDetailsReducer(undefined, {})).toEqual(ingDetailsInitialState);
   });
 
   it('should handle CLOSE_ING_MODAL', () => {
     const action = { type: CLOSE_ING_MODAL };
-    const expectedState = { ...initialState, isIngModalOpen: false };
-    expect(ingredientDetailsReducer(initialState, action)).toEqual(expectedState);
+    const expectedState = { ...ingDetailsInitialState, isIngModalOpen: false };
+    expect(ingredientDetailsReducer(ingDetailsInitialState, action)).toEqual(expectedState);
   });
 
   it('should handle OPEN_ING_MODAL', () => {
     const action = { type: OPEN_ING_MODAL, payload: { name: 'test' } };
-    const expectedState = { ...initialState, isIngModalOpen: true, item: { name: 'test' } };
-    expect(ingredientDetailsReducer(initialState, action)).toEqual(expectedState);
+    const expectedState = { ...ingDetailsInitialState, isIngModalOpen: true, item: { name: 'test' } };
+    expect(ingredientDetailsReducer(ingDetailsInitialState, action)).toEqual(expectedState);
   });
 });
